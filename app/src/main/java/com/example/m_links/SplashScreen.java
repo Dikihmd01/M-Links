3e0afe61605c696fe5c470126284b5afb5adc581
Dikihmd01 <dikihamdani.id@gmail.com>
2022-12-31 17:02:52 +0700

    Update splash screen

--- app/src/main/java/com/example/m_links/SplashScreen.java
+++ app/src/main/java/com/example/m_links/SplashScreen.java
@@ -2,13 +2,26 @@ package com.example.m_links;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
+
+    int duration = 2500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent route_login = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(route_login);
+                SplashScreen.this.finish();
+            }
+        }, duration);
     }
 }
